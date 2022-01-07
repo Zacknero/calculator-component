210b5ab2bb2f91e1cd93296ceb6ae7d60d558ebd
Zacknero <nerodevilman@live.it>
2022-01-07 19:06:04 +0100

    - final solution

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-
 @Component({
   selector: 'app-Calculator',
   templateUrl: './calculator.component.html',
@@ -14,6 +13,35 @@ export class CalculatorComponent implements OnInit {
   count: number;
 
   ngOnInit() {
+    this.symbol = '+';
+    this.count = 0;
   }
 
+  operationEvent(s: string) {
+    if (this.inp1 && this.inp2) {
+      this.symbol = s;
+      this.count += 1;
+      switch (s) {
+        case '+':
+          this.result = this.inp1 + this.inp2;
+          break;
+        case '-':
+          this.result = this.inp1 - this.inp2;
+          break;
+        case '*':
+          this.result = this.inp1 * this.inp2;
+          break;
+        case '/':
+          this.result = this.inp1 / this.inp2;
+          break;
+      }
+    }
+  }
+
+  restCalculator() {
+    this.symbol = '+';
+    this.result = null;
+    this.inp1 = null;
+    this.inp2 = null;
+  }
 }
